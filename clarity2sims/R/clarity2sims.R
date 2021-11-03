64b6a46c93236ebf8660581dbad6a43ba9d62376
James Totterdell <jatotterdell@gmail.com>
2021-11-03 14:57:07 +1100

    Simplify contrast summaries

--- clarity2sims/R/clarity2sims.R
+++ clarity2sims/R/clarity2sims.R
@@ -9,7 +9,7 @@ update_model <- function(mod, moddat, ...) {
   drp <- utils::capture.output(fit <- mod$sample(
     data = moddat, ...
   ))
-  draws <- posterior::as_draws_rvars(fit$draws(c("alpha", "beta")))
+  draws <- posterior::as_draws_rvars(fit$draws(c("alpha", "beta", "eta")))
   return(draws)
 }
 
@@ -36,35 +36,36 @@ sim_clarity2_trial <- function(mod,
                                n_seq = seq(600, 2100, 300),
                                p_assign = rep(1 / 3, 3),
                                alpha = stats::qlogis(cumsum(c(16, 29, 32, 13, 2, 1, 1, 6) / 100)[1:7]),
-                               eta = c(0, -0.5, 0.5),
+                               eta = c(0, 0, 0),
                                eff_eps = 0.975,
                                stage2 = "all",
-                               drop_arms = FALSE,
                                ...) {
+
   N <- length(p_assign)
   K <- length(alpha) + 1
   n_max <- max(n_seq)
   n_new <- diff(c(0, n_seq))
   n_int <- length(n_seq)
   y <- matrix(0, N, K)
-
-  # Contrast matrix
-  C <- rbind(c(1, 1), c(0, 1))
+  y_obs <- array(0,
+                 dim = c(n_int, N, K),
+                 dimnames = list("analysis" = seq_len(n_int),
+                                 "variable" = seq_len(N),
+                                 "level" = seq_len(K)))
 
   # Storage
-  labs <- list(c("analysis" = 1:n_int), c("arm" = 1:3))
+  labs <- list("analysis" = 1:n_int, "arm" = 1:3)
   n_obs <- matrix(0, n_int, N, dimnames = labs)
   e_alpha <- matrix(0, n_int, K - 1, dimnames = list(labs[[1]], c("cut" = 1:(K - 1))))
   v_alpha <- e_alpha
   e_beta <- matrix(0, n_int, N - 1, dimnames = list(labs[[1]], labs[[2]][-1]))
-  v_beta <- matrix(0, n_int, N - 1, dimnames = list(labs[[1]], labs[[2]][-1]))
-  pr_eff <- matrix(0, n_int, N - 1, dimnames = list(labs[[1]], labs[[2]][-1]))
-  pr_eff2 <- matrix(0, n_int, 1, dimnames = list(labs[[1]], c("arm" = 3)))
-  i_eff2 <- pr_eff2
+  v_beta <- e_beta
+  e_ctr <- matrix(0, n_int, 3, dimnames  = list("analysis" = seq_len(n_int), "contrast" = c("3v1", "3v2", "3v1and2")))
+  pr_ctr <- hi_ctr <- lo_ctr <- v_ctr <- e_ctr
 
   for (i in 1:n_int) {
     # After stage 1, if not "all" then drop one of the control arms
-    if (i == 2 & stage2 != "all") {
+    if (i == 2 & stage2 != "all" & N == 3) {
       if (stage2 == "first") {
         p_assign <- c(0.5, 0, 0.5)
       } else if (stage2 == "best" & e_beta[i - 1, 1] > 0) {
@@ -79,50 +80,50 @@ sim_clarity2_trial <- function(mod,
     y_new <- generate_data_agg(n_x, alpha, eta)
     y <- y + y_new
     mod[[2]]$y <- y
-    fit <- update_model(mod[[1]], mod[[2]], ...)
     n_obs[i, ] <- rowSums(y)
+    y_obs[i, , ] <- y
+
+    fit <- update_model(mod[[1]], mod[[2]], ...)
+    # Define contrasts
+    ctr <- c(fit$eta[3], fit$eta[3] - fit$eta[2], fit$eta[3] - posterior::rvar_max(c(fit$eta[1], fit$eta[2])))
+    # Summarise contrasts
+    e_ctr[i, ] <- posterior::E(ctr)
+    v_ctr[i, ] <- posterior::var(ctr)
+    tmp        <- posterior::quantile2(ctr, c(0.025, 0.975))
+    lo_ctr[i, ]<- tmp[1, ]
+    hi_ctr[i, ]<- tmp[2, ]
+    pr_ctr[i, ]<- posterior::Pr(ctr > 0)
+    # Other summaries
     e_alpha[i, ] <- posterior::E(fit$alpha)
     v_alpha[i, ] <- posterior::var(fit$alpha)
-    e_beta[i, ] <- posterior::E(fit$beta)
-    v_beta[i, ] <- posterior::var(fit$beta)
-    # is arm 2,3 better than arm 1?
-    pr_eff[i, ] <- posterior::Pr(fit$beta > 0)
-    # is arm 3 better than arm 1 and 2?
-    pr_eff2[i, ] <- posterior::Pr(fit$beta[2] > 0 & fit$beta[2] - fit$beta[1] > 0)
-    i_eff2[i, ] <- as.integer(pr_eff2[i, ] > eff_eps)
+    e_beta[i, ]  <- posterior::E(fit$beta)
+    v_beta[i, ]  <- posterior::var(fit$beta)
 
-    if (drop_arms) {
-
-    }
-
-    if (i_eff2[i, ] == 1) break
   }
-
   ind <- 1:i
 
-  # Summarise outputs by relevant groupings
   out_alpha <- list(
     e_alpha = e_alpha[ind, , drop = F],
     v_alpha = v_alpha[ind, , drop = F]
   )
-
   out_arm <- list(
     n_obs = n_obs[ind, , drop = F],
     e_beta = e_beta[ind, , drop = F],
-    v_beta = v_beta[ind, , drop = F],
-    pr_eff = pr_eff[ind, , drop = F],
-    pr_eff2 = pr_eff2[ind, , drop = F],
-    i_eff2 = i_eff2[ind, , drop = F]
+    v_beta = v_beta[ind, , drop = F]
   )
-
   out_ctr <- list(
-    pr_ctr = pr_ctr[ind, , drop = F],
-    ppos   = ppos[ind, , drop = F]
+    e_ctr = e_ctr[ind, , drop = F],
+    v_ctr = v_ctr[ind, , drop = F],
+    lo_ctr = lo_ctr[ind, , drop = F],
+    hi_ctr = hi_ctr[ind, , drop = F],
+    pr_ctr = pr_ctr[ind, , drop = F]
   )
+  y_obs <- y_obs[ind, , , drop = FALSE]
 
   return(list(
     alpha = list_as_dt(out_alpha),
     trial = list_as_dt(out_arm),
-    contr = list_as_dt(out_ctr)
+    contr = list_as_dt(out_ctr),
+    yobs  = dcast(array_as_dt(y_obs), ... ~ paste0("y", level), value.var = "y_obs")
   ))
 }
--- sims/run_clarity2sims.R
+++ sims/run_clarity2sims.R
@@ -3,7 +3,6 @@ library(cmdstanr)
 library(posterior)
 library(data.table)
 library(parallel)
-library(pbapply)
 library(optparse)
 
 #  ----- Command line arguments -----
@@ -11,7 +10,7 @@ option_list <- list(
   make_option(c("-c", "--cores"), type = "integer", default = 15,
               help = "number of cores to use",
               metavar = "character"),
-  make_option(c("-n", "--nsim"), type = "integer", default = 10,
+  make_option(c("-n", "--nsim"), type = "integer", default = 1000,
               help = "number of simulations to run under each configuration",
               metavar = "character")
 );
@@ -21,26 +20,32 @@ num_sims  <- opt$nsim
 
 #  ----- Compile the required model -----
 ordmod <- clarity2sims:::compile_cmdstanr_mod()
-ordmoddat <- list(N = 3, P = 2, K = 8,
-                  X = rbind(0, c(1, 0), c(1, 1)),
-                  prior_counts = rep(2 / 8, 8),
-                  prior_sd = rep(1, 2))
+ordmoddat <- list(
+  N = 3, P = 2, K = 8,
+  X = rbind(0, c(1, 0), c(1, 1)),
+  prior_counts = rep(2 / 8, 8),
+  prior_sd = rep(1, 2))
 mod <- list(ordmod, ordmoddat)
 
 #  ----- Randomisation -----
 RNGkind("L'Ecuyer-CMRG")
 set.seed(671359)
+mc.reset.stream()
 
 # ----- Specify configurations to explore -----
 cfg <- CJ(
   sims = num_sims,
-  n_seq = list(600, 2100),
+  n_seq = list(seq(600, 2100, 300)),
   eff_eps = 0.975,
-  eta = list(rep(0, 3)),
-  prior_counts = list(
-    5 * c(16, 29, 32, 13, 2, 1, 1, 6) / 100,
-    2 * rep(1 / 8, 8)),
-  prior_sd = list(rep(1, 2), rep(0.25, 2)),
+  alpha = list(qlogis(cumsum(c(16, 28, 32, 12, 2, 2, 2, 6) / 100)[1:7])),
+  eta = list(rep(0, 3),
+             c(0, 0, log(1/1.1)),
+             c(0, 0, log(1.1)),
+             c(0, 0, log(1.2)),
+             c(0, 0, log(1.3)),
+             c(0, 0, log(1.5)),
+             c(0, 0, log(2.0))),
+  stage2 = c("all", "first", "best"),
   sorted = FALSE
 )
 
@@ -56,21 +61,26 @@ for (z in run_row) {
       mod,
       n_seq = unlist(cfg[z][["n_seq"]]),
       eff_eps = cfg[z][["eff_eps"]][[1]],
+      alpha = cfg[z][["alpha"]][[1]],
       eta = cfg[z][["eta"]][[1]],
+      stage2 = cfg[z][["stage2"]],
       refresh = 0)
   }, mc.cores = num_cores)
 
   resl_alpha <- rbindlist(lapply(res, \(x) x[["alpha"]]), idcol = "trial")
   resl_trial <- rbindlist(lapply(res, \(x) x[["trial"]]), idcol = "trial")
+  resl_contr <- rbindlist(lapply(res, \(x) x[["contr"]]), idcol = "trial")
   resl_alpha[, analysis := as.numeric(analysis)]
   resl_trial[, analysis := as.numeric(analysis)]
+  resl_contr[, analysis := as.numeric(analysis)]
 
   end_time <- Sys.time()
 
   saveRDS(list(cfg = cfg[z],
                alpha = resl_alpha,
                trial = resl_trial,
+               contr = resl_contr,
                runtime = end_time - start_time),
-          paste0("~/out_files/clarity2_sims/fixed_ss_",
+          paste0("~/out_files/clarity2_sims/pprob_",
                  formatC(z, width = 2, flag = "0"), ".rds"))
 }
